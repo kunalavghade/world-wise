8fed3990642a1b7db4a5104e4b38e87c3a73e6b3
kunalavghade <kunalavghade77@gmail.com>
2023-08-14 14:00:45 +0530

    styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Homepage from "./pages/HomePage";
 import Product from "./pages/Product";
-import Pricing from "./pages/Pricing";
+// import Pricing from "./pages/Pricing";
 import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
+import Pricing from "./pages/Pricing";
 
 function App() {
   return (
@@ -11,6 +14,8 @@ function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
+        <Route path="app" element={<AppLayout />} />
+        <Route path="login" element={<Login />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
